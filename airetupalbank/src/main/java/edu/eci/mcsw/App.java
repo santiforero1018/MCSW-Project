c273e6ce619a50af515fd0d2afe54080ca03f5dd
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2024-02-25 19:31:41 -0500

    estructura base del proyecto

---
+++ airetupalbank/src/main/java/edu/eci/mcsw/App.java
@@ -0,0 +1,28 @@
+package edu.eci.mcsw;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import edu.eci.mcsw.persistence.DbConnection;
+import edu.eci.mcsw.server.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+        WebServer.startServer();
+        Connection con = DbConnection.getConnection();
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println("Couldn't start the server: " + e.getMessage());
+        } catch(SQLException e){
+            System.out.println("Couldn Connect to Database: " + e.getMessage());
+        }
+    }
+}
